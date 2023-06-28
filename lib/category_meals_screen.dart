e866afd6ea5b1080d623ee2be0694d265b1fa398
olga_tim <tigercub220@gmail.com>
2023-06-28 17:12:09 +0300

    Add selecting meals for a chosen category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   // final String cattegoryId;
   // final String categoryTitle;
@@ -16,13 +18,20 @@ class CategoryMealsScreen extends StatelessWidget {
     final categoryId = routeArgs['id'];
     final categoryColor = routeArgs['color'];
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
         backgroundColor: categoryColor,
       ),
       body: ListView.builder(
-        itemBuilder: (ctx, index) {},
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
